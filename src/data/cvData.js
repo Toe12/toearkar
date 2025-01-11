0b01bf7bdf96a127ad887868f7c0aa4280f2295a
Toe Arkar <toearkar.15@gmail.com>
2025-01-10 22:54:53 -0500

    first commit

---
+++ src/data/cvData.js
@@ -0,0 +1,100 @@
+export const contact = {
+  email: "toearkar@protonmail.com",
+  linkedin: "https://www.linkedin.com/in/toe-arkar-504b40248/",
+  github: "https://github.com/Toe12",
+};
+
+export const education = [
+  {
+    degree: "Bachelor of Science, Computer Science",
+    institution: "Webster University",
+    location: "Saint Louis, MO, United States",
+    dateRange: "October 2016 – August 2020",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Founding Software Engineer",
+    company: "We Care Insurance Inc.",
+    dateRange: "October 2022 – Present",
+    responsibilities: [
+      "Participated in code reviews, unit testing, integration testing, and debugging to deliver quality software and maintain a Spring Boot application.",
+      "Built a scalable cloud architecture using Oracle Cloud Infrastructure, achieving significant server cost reductions by utilizing OCI listeners, load balancers, and VMs for enhanced efficiency.",
+      "Utilized OCI listeners, load balancers, and VMs to achieve this efficiency.",
+      "Successfully migrated the JavaScript UI codebase to the React codebase.",
+      "Implemented caching with Redis to optimize performance by storing retrieved endpoint data.",
+      "Integrated RabbitMQ to streamline email services, enabling reliable, asynchronous email delivery and enhancing system responsiveness.",
+      "Successfully recruited a talented and seasoned software consultant to bring industry expertise and strategic insights into the project.",
+      "Recruited and mentored interns to boost their technical skills and team performance.",
+      "Shipped products and onboarded new users to ensure a smooth transition and satisfaction.",
+    ],
+  },
+  {
+    title: "Software Engineer",
+    company: "We Care Insurance Inc.",
+    dateRange: "April 2021 – September 2022",
+    responsibilities: [
+      "Built full-stack web applications with Spring Boot, using JavaScript, jQuery for dynamic UI features, and Axios to connect with backend REST APIs.",
+      "Participated in migrating JDBC code to JPA Hibernate and refactored the codebase to improve performance and efficiency.",
+      "Wrote unit test cases using JUnit and Mockito and used MockMvc and TestContainers for integration testing with a Behavior-Driven Development (BDD) approach.",
+      "Set up role-based access controls to improve user permissions and security with Spring Security.",
+      "Implemented transaction management in Spring Boot to maintain data consistency and meet ACID standards.",
+      "Implemented containerization of applications and databases, and established GitHub Actions CI/CD pipelines for automated testing and deployment on Oracle Cloud Infrastructure.",
+    ],
+  },
+  {
+    title: "Software Engineer Internship",
+    company: "We Care Insurance Inc.",
+    dateRange: "September 2020 – March 2021",
+    responsibilities: [
+      "Developed technical specifications from business requirements, ensuring alignment of software projects with strategic objectives.",
+      "Designed and administered SQL database schemas, facilitating the functionality of application systems.",
+      "Improved web application features by integrating JDBC, XML, and JavaMail, enhancing user experience and communication functions.",
+      "Integrated third-party APIs for VIN (Vehicle Identification Number) validation and address verification, alongside Amazon S3, to facilitate the uploading of insurance documents.",
+    ],
+  },
+  {
+    title: "Database Developer Internship",
+    company: "Forkaia",
+    dateRange: "June 2019 – August 2019",
+    responsibilities: [
+      "Collaborated with Business Analysts to create functional specifications and translate them into technical specifications.",
+      "Designed and implemented relational databases to store all user information.",
+      "Normalized relational table designs up to the fourth normal form to improve data integrity and reduce redundancy.",
+      "Created Entity-Relationship (ER) models to map out data relationships and interactions.",
+      "Developed Triggers, Tables, Indexes, Views, and Queries for efficient data retrieval and processing.",
+      "Built a database application using Python to support data management and analysis.",
+    ],
+  },
+];
+
+export const personalProjects = [
+  {
+    name: "Insurmate.app",
+    description: `
+        Leading a private blockchain solution using Hyperledger Fabric for policy 
+        management and exchange in the insurance industry. Mentored a Fall 2024 
+        capstone team at the University of Nebraska Omaha, delivering an MVP with 
+        an Express.js backend for efficient blockchain interactions.
+      `,
+    posterLink: "https://www.insurmate.app/uno/fall_2024_poster.pdf",
+  },
+];
+
+export const skills = {
+  languages: ["Java", "JavaScript"],
+  databases: ["MongoDB", "MySQL", "Redis"],
+  versionControl: ["Bitbucket", "GitHub", "Jira"],
+  frameworks: [
+    "React",
+    "Hyperledger Fabric",
+    "Spring Boot",
+    "Spring Data JPA",
+    "Mockito",
+    "JUnit",
+    "Bootstraps",
+  ],
+  cloud: ["Oracle Cloud Infrastructure (OCI)", "AWS S3"],
+  containerization: ["Docker"],
+};
